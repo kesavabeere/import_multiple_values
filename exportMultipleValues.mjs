171ee09935dee691b856cd79dacbba4ac5f4bf3d
kesava <beerekesava007@gmail.com>
2023-01-12 05:55:55 +0000

    multiple_value

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,7 @@
+export function multiplyByFour(number) {
+  return number * 4;
+}
+
+export let bulb = { watts: 10, type: "LED" };
+
+export let myArray = ["camel", 265, true, "5.6"];
---
+++ importMultipleValues.mjs
@@ -0,0 +1,7 @@
+import { multiplyByFour, bulb, myArray } from "./exportMultipleValues.mjs";
+
+console.log(multiplyByFour(4));
+
+console.log(bulb);
+
+console.log(myArray);
